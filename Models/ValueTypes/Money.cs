cf4163dee07c893e697c888a1bab134977781993
Enrico Astolfi <astolfie@tiscali.it>
2022-03-27 18:08:08 +0200

    Money

--- Models/ValueTypes/Money.cs
+++ Models/ValueTypes/Money.cs
@@ -5,56 +5,37 @@ namespace MyCourse.Models.ValueTypes
 {
     public class Money
     {
-        private Currency eUR;
-        private decimal v;
-
-        public Money() : this(Currency.EUR , 0.00m) 
+        public Money() : this(Currency.EUR, 0.00m)
         {
-
         }
-
         public Money(Currency currency, decimal amount)
         {
             Amount = amount;
             Currency = currency;
         }
         private decimal amount = 0;
-        public decimal Amount 
-        {
-             get 
-             {
-                 return amount;
-             }
-             set 
-             {
-                 if (value < 0) {
-                     throw new InvalidOperationException("The amount cannot be negative");
-                 }
-                 amount = value;
-             } 
+        public decimal Amount
+        { 
+            get
+            {
+                return amount;
+            }
+            init
+            {
+                if (value < 0) {
+                    throw new InvalidOperationException("The amount cannot be negative");
+                }
+                amount = value;
+            }
         }
-        public Currency Currency 
+        public Currency Currency
         {
-            get; set;
+            get; init;
         }
-
-        public override bool Equals(object obj)
-        {
-            var money = obj as Money;
-
-            return money != null &&
-                    Amount == money.Amount && 
-                    Currency == money.Currency;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Amount, Currency);
-        }
-
+        
         public override string ToString()
         {
-            return $"{Currency} {Amount:#.00}";
+            return $"{Currency} {Amount:0.00}";
         }
     }
 }
