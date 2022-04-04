1175e30155be7c2dbbad6dcf9192954c98ea5b5e
Enrico Astolfi <astolfie@tiscali.it>
2022-04-04 09:29:20 +0200

    transient

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyCourse.Models.Services.Application;
 
 namespace MyCourse
 {
@@ -17,6 +18,7 @@ namespace MyCourse
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc(options=> options.EnableEndpointRouting = false);
+            services.AddTransient<CourseService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
